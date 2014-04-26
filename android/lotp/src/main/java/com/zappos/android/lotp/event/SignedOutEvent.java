e5e3811a606db15883377247463d123c2f2458fe
Matthew Runo <matthew.runo@gmail.com>
2014-04-25 22:25:47 -0700

    Android build..

--- android/lotp/src/main/java/com/zappos/android/lotp/event/SignedOutEvent.java
+++
@@ -1,7 +0,0 @@
-package com.zappos.android.lotp.event;
-
-/**
- * Created by mattkranzler on 12/5/13.
- */
-public class SignedOutEvent {
-}
--- android/lotp/src/main/java/com/zappos/android/lotp/fragment/LoginFragment.java
+++
@@ -1,9 +0,0 @@
-package com.zappos.android.lotp.fragment;
-
-import android.app.Fragment;
-
-/**
- * Created by mattkranzler on 12/4/13.
- */
-public class LoginFragment extends Fragment {
-}
